40fbdd0ba066cd9b98a2fecaa72a2cfc8bbdfd02
dgarcia024 <danielgv2404@gmail.com>
2025-01-09 10:18:40 -0600

    first commit

---
+++ src/HolaMundo.java
@@ -0,0 +1,17 @@
+/**
+ * proyecto: HolaMundoJv
+ * creado por: D_GARCIA
+ */
+public class HolaMundo {
+
+     public static void main(String[] args) {
+         String saludar = "Daniel Garcia V";
+         System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
+
+         int numero = 10;
+         boolean valor = true;
+         if(valor) {
+             System.out.println("numero = " + numero);
+         }
+    }
+}
---
+++ src/NombreAlumno.java
@@ -0,0 +1,9 @@
+/**
+ * proyecto: HolaMundoJv
+ * creado por: D_GARCIA
+ */
+public class NombreAlumno {
+    public static void main(String[] args) {
+        System.out.println("Daniel Garcia");
+    }
+}
